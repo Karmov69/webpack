34461866ba6630ccc00bd3b73b1d6dc43872ba52
Karmov Arthur <Artur69Karmov@gmail.com>
2020-01-17 19:24:22 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import * as $ from "jquery";
+import Post from "@models/Post";
+import WebpackLogo from "./assets/webpack-logo.png";
+// import json from "./assets/json.json";
+// import xml from "./assets/data.xml";
+// import csv from "./assets/data.csv";
+import "./babel";
+import "./styles/styles.css";
+import "./styles/sass.scss";
+
+const post = new Post("Webpack Post Title", WebpackLogo);
+
+$("pre")
+  .addClass("code")
+  .html(post.toString());
+
+// console.log("JSON:", json);
+// console.log("XML:", xml);
+// console.log("CSV:", csv);
